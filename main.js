71923ab898b7df47fdff06aaf960b1692b59e849
Saveleva <saveleva_ol@mail.ru>
2019-01-25 22:42:38 +0300

    Created work-2. Again

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 //------------Work-1-------------//
 
-//var a=6;//просто на всякий случай
-//function printWindowValues(){
+// var a=6;//просто на всякий случай
+// function printWindowValues(){
 //  console.log(window);
-//}
-//printWindowValues();
+// }
+// printWindowValues();
 
-//------------Work-2-------------//
-// function expTen(x,ch){
+// ------------Work-2-------------//
+// function expTen(ch){
+//   var x=1;
 //  for(var i=1;i<11;i++){
 //    x=x*ch;
 //  }
 // console.log(x);
 // }
-// expTen(1,5);
+// expTen(2);
+
+// Пример этого же задания через рекурсию
+
+// function expTen(i,x)
+// {
+//   if(i>1)
+//   {
+//     x=x*expTen(i-1,x);
+//   }
+//   return x;
+//   console.log(x);
+// }
+// console.log(expTen(10,2));
 
 //------------Work-3-------------//
+
+// function getFuncRes(outPut(),str){
+// console.log(str)
+// }
