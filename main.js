d279b1b39b264ea4a572d096d65877283335fc63
Saveleva <saveleva_ol@mail.ru>
2019-01-25 19:43:16 +0300

    Created work-2

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 //------------Work-1-------------//
 
-var a=6;//просто на всякий случай
-function printWindowValues(){
-  console.log(window);
-}
-printWindowValues();
+//var a=6;//просто на всякий случай
+//function printWindowValues(){
+//  console.log(window);
+//}
+//printWindowValues();
 
 //------------Work-2-------------//
+// function expTen(x,ch){
+//  for(var i=1;i<11;i++){
+//    x=x*ch;
+//  }
+// console.log(x);
+// }
+// expTen(1,5);
+
+//------------Work-3-------------//
