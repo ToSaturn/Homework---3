2cbccc91cec4321a630f3d32a78eb05190164a5f
Saveleva <saveleva_ol@mail.ru>
2019-01-25 19:12:15 +0300

    Created work-1

---
+++ main.js
@@ -0,0 +1,9 @@
+//------------Work-1-------------//
+
+var a=6;//просто на всякий случай
+function printWindowValues(){
+  console.log(window);
+}
+printWindowValues();
+
+//------------Work-2-------------//
