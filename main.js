868c4a2a0065a6649167194e4fca999a85df2b96
Saveleva <saveleva_ol@mail.ru>
2019-01-26 01:43:34 +0300

    Все работы доделаны

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
 //------------Work-1-------------//
 
-// var a=6;//просто на всякий случай
-// function printWindowValues(){
-//  console.log(window);
-// }
-// printWindowValues();
+var arr=[1,2,3,4,5,6];
+function printWindowValues(){
+ console.log(window.arr); //я и то, и то вывожу, потому что
+ console.log(window);     //не уверена, что поняла задание
+}
+printWindowValues();
 
 // ------------Work-2-------------//
-// function expTen(ch){
-//   var x=1;
-//  for(var i=1;i<11;i++){
-//    x=x*ch;
-//  }
-// console.log(x);
-// }
-// expTen(2);
+
+function expTen(ch){
+  var x=1;
+ for(var i=1;i<11;i++){
+   x=x*ch;
+ }
+console.log(x);
+}
+expTen(2);
 
 // Пример этого же задания через рекурсию
 
-// function expTen(i,x)
-// {
-//   if(i>1)
-//   {
-//     x=x*expTen(i-1,x);
-//   }
-//   return x;
-//   console.log(x);
-// }
-// console.log(expTen(10,2));
+function expTen(i,x)
+{
+  if(i>1)
+  {
+    x=x*expTen(i-1,x);
+  }
+  return x;
+  console.log(x);
+}
+console.log(expTen(10,2));
 
 //------------Work-3-------------//
 
-// function getFuncRes(outPut(),str){
-// console.log(str)
-// }
+function getFuncRes(a,b){
+a(b);
+}
+getFuncRes(console.log,"Hello World!");
